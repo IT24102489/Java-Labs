ba655d5f257d50a36f72ddb87e108acf9b661e84
IT24102489 <it24102489@my.sliit.lk>
2024-07-31 23:35:21 +0530

    Add files via upload

---
+++ Prathibha.java
@@ -0,0 +1,10 @@
+public class Prathibha{
+	public static void main(String[] args){
+		System.out.println("ai mokathe");
+		System.out.println("blnne");
+		System.out.println("een parkkiraaii");
+		System.out.println("enakku thiratchei wendummmm");
+		System.out.println(":)");
+		
+	}
+}
